2770e510eeeac504c6ca4524a36ac20545d4448c
solena8 <solena_ananda@hotmail.com>
2025-01-22 14:32:50 +0100

    difficulty text

--- back_natena/src/network/routes_api.py
+++ back_natena/src/network/routes_api.py
@@ -42,84 +42,35 @@ def get_spot_list_data():
     except sqlite3.Error as e:
         raise HTTPException(status_code=500, detail=f"Failed to fetch data: {e}")
 
-# Version avec plusieurs images, qui ne matche pas avec le front
-@app.get("/spot/{id}")
-def get_details_for_a_spot(id: int):
-    spotQuery = """
-        SELECT
-            s.id, sb.type, s.address, s.geocode, s.difficulty
-        FROM
-            spot s
-        JOIN
-            surf_break sb ON sb.id = s.surf_break_id
-        WHERE
-            s.id = ?
-    """
-    imageQuery = """
-        SELECT * FROM image i
-        WHERE i.spot_id = ?
-    """
-    try:
-        with get_db() as conn:
-            conn.row_factory = Row
-            cur = conn.cursor()
-            cur.execute(spotQuery, (id,))
-            # id en parametre permet de définir le ? de s.id = ?
-            # Utilisation de paramètres liés pour éviter l'injection SQL
-            spotRows = cur.fetchall()
-
-            # Transformer les résultats de la requête spot en liste de dictionnaires
-            spotData = []
-            for row in spotRows:
-                row_dict = dict(row)
-                try:
-                    latitude, longitude = SpotDto.convertGeocode(row_dict.pop("geocode"))
-                    row_dict["latitude"] = latitude
-                    row_dict["longitude"] = longitude
-                except ValueError as e:
-                    raise HTTPException(status_code=500, detail=f"Erreur avec le geocode: {e}")
-                spotData.append(row_dict)
-
-            # Transformer les résultats de la requête image en liste de dictionnaires
-            cur.execute(imageQuery, (id,))
-            imageRows = cur.fetchall()
-            imageData = []
-            for row in imageRows:
-                row_dict = dict(row)
-                imageData.append(row_dict)
-
-            return {
-                "spot": spotData,
-                "images": imageData
-                    }
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Failed to fetch data: {e}")
-
-
-# Version avec une seule image, qui match avec le front
+# # Version avec plusieurs images, qui ne matche pas avec le front
 # @app.get("/spot/{id}")
 # def get_details_for_a_spot(id: int):
-#     query = """
+#     spotQuery = """
 #         SELECT
-#             s.id, sb.type, s.address, i.url, i.main, s.geocode, s.difficulty
+#             s.id, sb.type, s.address, s.geocode, s.difficulty
 #         FROM
 #             spot s
 #         JOIN
 #             surf_break sb ON sb.id = s.surf_break_id
-#         JOIN
-#             image i ON i.spot_id = s.id
 #         WHERE
 #             s.id = ?
+#     """
+#     imageQuery = """
+#         SELECT * FROM image i
+#         WHERE i.spot_id = ?
 #     """
 #     try:
 #         with get_db() as conn:
 #             conn.row_factory = Row
 #             cur = conn.cursor()
-#             cur.execute(query, (id,))  # Utilisation de paramètres liés pour éviter l'injection SQL
-#             rows = cur.fetchall()
-#             # Transformer les résultats en liste de dictionnaires
-#             data = []
-#             for row in rows:
+#             cur.execute(spotQuery, (id,))
+#             # id en parametre permet de définir le ? de s.id = ?
+#             # Utilisation de paramètres liés pour éviter l'injection SQL
+#             spotRows = cur.fetchall()
+#
+#             # Transformer les résultats de la requête spot en liste de dictionnaires
+#             spotData = []
+#             for row in spotRows:
 #                 row_dict = dict(row)
 #                 try:
 #                     latitude, longitude = SpotDto.convertGeocode(row_dict.pop("geocode"))
@@ -127,10 +78,59 @@ def get_details_for_a_spot(id: int):
 #                     row_dict["longitude"] = longitude
 #                 except ValueError as e:
 #                     raise HTTPException(status_code=500, detail=f"Erreur avec le geocode: {e}")
-#                 data.append(row_dict)
-#             return {"spots": data}
+#                 spotData.append(row_dict)
+#
+#             # Transformer les résultats de la requête image en liste de dictionnaires
+#             cur.execute(imageQuery, (id,))
+#             imageRows = cur.fetchall()
+#             imageData = []
+#             for row in imageRows:
+#                 row_dict = dict(row)
+#                 imageData.append(row_dict)
+#
+#             return {
+#                 "spot": spotData,
+#                 "images": imageData
+#                     }
 #     except Exception as e:
 #         raise HTTPException(status_code=500, detail=f"Failed to fetch data: {e}")
+
+
+# Version avec une seule image, qui match avec le front
+@app.get("/spot/{id}")
+def get_details_for_a_spot(id: int):
+    query = """
+        SELECT
+            s.id, sb.type, s.address, i.url, i.main, s.geocode, s.difficulty
+        FROM
+            spot s
+        JOIN
+            surf_break sb ON sb.id = s.surf_break_id
+        JOIN
+            image i ON i.spot_id = s.id
+        WHERE
+            s.id = ?
+    """
+    try:
+        with get_db() as conn:
+            conn.row_factory = Row
+            cur = conn.cursor()
+            cur.execute(query, (id,))  # Utilisation de paramètres liés pour éviter l'injection SQL
+            rows = cur.fetchall()
+            # Transformer les résultats en liste de dictionnaires
+            data = []
+            for row in rows:
+                row_dict = dict(row)
+                try:
+                    latitude, longitude = SpotDto.convertGeocode(row_dict.pop("geocode"))
+                    row_dict["latitude"] = latitude
+                    row_dict["longitude"] = longitude
+                except ValueError as e:
+                    raise HTTPException(status_code=500, detail=f"Erreur avec le geocode: {e}")
+                data.append(row_dict)
+            return {"spots": data}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to fetch data: {e}")
 
 @app.post("/spots")
 def insert_new_spot():
